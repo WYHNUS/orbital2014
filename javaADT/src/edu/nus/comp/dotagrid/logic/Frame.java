e5748116744904067e06dfd3b31d74d018895d20
dingxiangfei2009 <dingxiangfei2009@gmail.com>
2014-05-24 18:35:55 +0800

    Test, again

---
+++ javaADT/src/edu/nus/comp/dotagrid/logic/Frame.java
@@ -0,0 +1,18 @@
+package edu.nus.comp.dotagrid.logic;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7654968618843340259L;
+	public Frame () {
+		super();
+		System.out.println("Test");
+	}
+	public static void main (String[] args) {
+		
+	}
+}
