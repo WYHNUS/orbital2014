8852ca32378c1a000d120a5b96b2637c788c3a94
dingxiangfei2009 <apple@appletekiMacBook-Air.local>
2014-06-08 01:07:10 +0800

    Moving to Swift

---
+++ DotAGridMobile/DotAGridMobile/MainGLView.swift
@@ -0,0 +1,15 @@
+//
+//  MainGLView.swift
+//  DotAGridMobile
+//
+//  Created by apple on 8/6/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+import Foundation
+
+class MainGLView {
+    func init () {
+        
+    }
+}
