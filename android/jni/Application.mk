a0ad869f639d65b0c882adfba5627d5165376b86
dingxiangfei2009 <dingxiangfei2009@gmail.com>
2014-06-17 17:53:51 +0800

    Prepare for C++11

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,6 @@
 APP_ABI := armeabi armeabi-v7a
-APP_STL := stlport_static
+APP_STL := gnustl_static
+APP_CPPFLAGS += -std=c++11
 APP_CPPFLAGS += -frtti
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -DANDROID
