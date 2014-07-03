e277d5b09524407198dbaf16ccdb8ce05c404e32
dingxiangfei2009 <dingxiangfei2009@gmail.com>
2014-07-03 18:00:26 +0800

    Rule books

---
+++ android/src/edu/nus/comp/dotagridandroid/logic/HeroRulebook.java
@@ -0,0 +1,21 @@
+package edu.nus.comp.dotagridandroid.logic;
+
+import java.util.Map;
+
+public class HeroRulebook {
+	public static void applyRule(GameState stateMachine, String character, String actionName, Map<String, Object> options) {
+		switch (actionName) {
+		case "GameAction": {
+			switch ((String) options.get("Action")) {
+			case "Move":
+			case "BuyItem":
+			case "SellItem":
+			case "Skill":
+			case "Attack":
+				break;
+			}
+			break;
+		}
+		}
+	}
+}
---
+++ android/src/edu/nus/comp/dotagridandroid/logic/LinecreepAutomation.java
@@ -0,0 +1,8 @@
+package edu.nus.comp.dotagridandroid.logic;
+
+public class LinecreepAutomation {
+	public static void processRound(GameState stateMachine, GameMaster master,
+			String charName) {
+		
+	}
+}
